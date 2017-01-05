e691822392181cbb230b5823b639e0dea4dfdc8e
José Ignacio Orlando <ignaciorlando@gmail.com>
2017-01-05 15:35:37 -0300

    Updated getFD

--- matlab/getFD.m
+++ matlab/getFD.m
@@ -21,26 +21,46 @@ if (size(img,3) > 1)
 end
 
 % if range is 0..1, scale to 0 ..255:
-if (max(img(:)) < 1)
+if (max(img(:)) <= 1)
     img = 255 * img;
 end
 img = uint8(img);
-[h, w] = size(img);
 
-lmax = min([w,h,64]);
+
+width = max(size(img));    % largest size of the box
+p = log(width)/log(2);   % nbre of generations
+
+% remap the array if the sizes are not all equal,
+% or if they are not power of two
+% (this slows down the computation!)
+if p~=round(p) || any(size(img)~=width)
+    % get the highest power of 2
+    p = ceil(p);
+    % compute the new width
+    width = 2^p;
+    % generate the new image
+    mz = uint8(zeros(width, width));
+    % put current image in the very corner of the new image
+    mz(1:size(img,1), 1:size(img,2)) = img;
+    % reassign to c, c_inf and c_cor
+    img = mz;
+end
+
+[h, w] = size(img);
+lmax = 2^p;
 
 wn = floor(w / lmax) * lmax;
 hn = floor(h / lmax) * lmax;
 
 
 %fprintf(1,'Calculating over h=%d and w=%d\n', hn, wn);
-L = [];
-NL = [];
-InfL = [];
-SqrFreqL = [];
+L = zeros(p,1);
+NL = zeros(p,1);
+InfL = zeros(p,1);
+SqrFreqL = zeros(p,1);
 count = 0;
 hnwn = hn*wn;
-loghw = log(hnwn);
+S = log(hnwn); % number of pixels, S in Reuters thesis
 for boxsize = pow2(1:floor(log2(lmax)))
     Nsum = 0;
     i = 0;
@@ -55,9 +75,17 @@ for boxsize = pow2(1:floor(log2(lmax)))
         mini = single(min(ibox(:)));
         %fprintf('max %d  min %d   bs %d  add %d\n',maxi,mini,boxsize,floor((maxi - mini) / boxsize)+1);
         N = floor((maxi - mini) / boxsize)+1;  %number of boxes (round up)
+        
+        % accumulate the number of boxes
         Nsum = Nsum+ N; 
-        Inf = Inf + bs2 * (log(bs2 / N) - loghw );
+        
+        
+        Inf = Inf + bs2 * (log(bs2 / N) - S );
+        
+        
         SqrFreq = SqrFreq + bs2 *bs2 / N;
+        
+        
         i = i+1;    % count squares
       end
     end
@@ -75,36 +103,21 @@ n_inf = InfL;
 n_corr = log(SqrFreqL);
 
 % istart = 2;
-% iend = size(results,2)-0;
-% X = [ ones(iend-istart+1,1) results(1,istart:iend)'];
+% iend = length(NL);
+% X = [ ones(iend-istart+1,1) log(L(istart:iend))];
 % 
 % logNL = -log(NL);
-% Y = logNL(istart:iend)';
+% Y = logNL(istart:iend);
 % [B,BINT,R,RINT, STATS] = regress(Y,X);
 % FDcap = B(2);
 % 
-% Y = InfL(istart:iend)';
+% Y = InfL(istart:iend);
 % [B,BINT,R,RINT, STATS] = regress(Y,X);
 % FDinf = B(2);
 % 
 % logsqr = log(SqrFreqL);
-% Y = logsqr(istart:iend)';
+% Y = logsqr(istart:iend);
 % [B,BINT,R,RINT, STATS] = regress(Y,X);
 % FDcor = B(2);
-
-%fprintf('\nCapacity Fractal Dimension    : %0.5g\nInformation Fractal Dimension : %0.5g\nCorrelation Fractal Dimension : %0.5g\n',FDcap,FDinf, FDcor);
-
-return
-
-% % Plot linear fit
-% figure; hold on;
-% %plot(results(1,1:istart-1), results(2,1:istart-1), 'r.')
-% plot(results(1,istart:iend), results(2,istart:iend), 'b.')
-% %plot(results(1,iend+1:end), results(2,iend+1:end), 'r.')
-% Xfit = results(1,istart:iend);
-% Yfit = B(1) + B(2) * Xfit;
-% plot(Xfit,Yfit,':k');
 % 
-% xlabel('ln(L)');
-% ylabel('-ln(N)');
-% title(['Prob. Fractal Dim.: ' sprintf('%0.4g (corr: %0.3g)',FD,corr(Y,Yfit'))]);
+% fprintf('\nCapacity Fractal Dimension    : %0.5g\nInformation Fractal Dimension : %0.5g\nCorrelation Fractal Dimension : %0.5g\n',FDcap,FDinf, FDcor);
